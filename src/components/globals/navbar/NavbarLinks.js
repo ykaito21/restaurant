80458585f60ab4bdbb943c7be9851eebf111e080
Yuki Kaito <ykaito21@gmail.com>
2019-02-21 02:29:22 +0900

    stop at nav before complete link

---
+++ src/components/globals/navbar/NavbarLinks.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class NavbarLinks extends Component {
+  render() {
+    return (
+      <div>
+        <h1>navbar link</h1>
+      </div>
+    );
+  }
+}
+
+export default NavbarLinks;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
+import Navbar from "../components/globals/navbar";
 import PropTypes from "prop-types";
 
 import "./layout.css";
 
 const Layout = ({ children }) => {
-  return <React.Fragment>{children}</React.Fragment>;
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <Navbar />
+      {children}
+    </React.Fragment>
+  );
 };
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Open Sans', 'sans-serif';
+  background: #fff
+}
+
+`;
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired
