e860f9fe55a800bb4cc02b58a28fc5d73d9210f1
Yuki Kaito <ykaito21@gmail.com>
2019-02-21 19:44:48 +0900

    still link

--- src/components/globals/navbar/NavbarLinks.js
+++ src/components/globals/navbar/NavbarLinks.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { Link } from "gatsby";
+import styled from "styled-components";
+import { styles } from "../../../utils";
 
 class NavbarLinks extends Component {
   render() {
