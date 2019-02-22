7e1ff38761e895fb9a2eef5c178c46a07ef2a68a
Yuki Kaito <ykaito21@gmail.com>
2019-02-22 22:34:12 +0900

    setup contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
 module.exports = {
   siteMetadata: {
     title: `Zaffran`,
@@ -15,6 +19,14 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.app/env-vars
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN
+      }
+    },
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
